2f8ddb30f81f932f9ad95f8324ee232c13f3f0a4
Luke Schlangen <lukeschlangen@gmail.com>
2023-12-02 13:56:13 -0600

    add react form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
+
 function App() {
+
+  const [displayName, setDisplayName] = useState('');
+  const [messageText, setMessageText] = useState('');
+
+  const sendMessage = () => {
+    const newMessage = {
+      userName: displayName,
+      text: messageText,
+    };
+    console.log(newMessage);
+  }
 
   return (
     <>
       <div>Baby Google Mama Serverless Backend</div>
+      <input value={displayName} onChange={(event) => setDisplayName(event.target.value)} />
+      <input value={messageText} onChange={(event) => setMessageText(event.target.value)} />
+      <button onClick={sendMessage}>Send Message</button>
     </>
   )
 }
